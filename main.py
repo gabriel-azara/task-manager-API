219f44569beb4a00f663fe98ca500e007f228d2d
gabriel <gabriel1190@gmail.com>
2026-04-20 11:21:12 -0300

    refactor: improve code readability by removing redundant whitespace and simplifying task creation logic

--- main.py
+++ main.py
@@ -4,39 +4,42 @@ from typing import List
 
 app = FastAPI()
 
+
 class Task(BaseModel):
     id: int
     title: str
     done: bool = False
 
+
 class TaskCreate(BaseModel):
     title: str = Field(min_length=1)
 
+
 tasks: List[Task] = []
 current_id = 1
+
 
 @app.get("/")
 def home():
     return {"message": "Rodando API de tarefas!"}
 
+
 @app.get("/tasks")
 def get_tasks():
     return tasks
 
+
 @app.post("/tasks")
 def create_task(task: TaskCreate):
     global current_id
-    
-    new_task = Task(
-        id=current_id,
-        title=task.title,
-        done=False
-    )
-    
+
+    new_task = Task(id=current_id, title=task.title, done=False)
+
     tasks.append(new_task)
     current_id += 1
-    
+
     return new_task
+
 
 @app.put("/tasks/{task_id}")
 def update_task(task_id: int):
@@ -45,6 +48,7 @@ def update_task(task_id: int):
             task.done = True
             return task
     raise HTTPException(status_code=404, detail="Tarefa não encontrada")
+
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
